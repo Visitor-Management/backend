4f1c68e73735ce634f3c9c6b6ccdceb08e2971ef
Archis Bhoir <archisbhoir2016@gmail.com>
2021-03-31 17:52:53 +0530

    Basic Config

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
 
 const app = express()
 
-app.get("/",(req,res)=>{
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
     res.send('hii harshal')
 })
 
-app.listen(8000)
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => console.log(`::::Server Running at port ${PORT}`))
