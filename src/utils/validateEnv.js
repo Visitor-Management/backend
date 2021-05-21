39046b2747827fcb196342221dc59cc080c942ab
Archis Bhoir <archisbhoir2016@gmail.com>
2021-05-21 12:37:39 +0530

    Env validation removed for heroku

--- src/utils/validateEnv.js
+++ src/utils/validateEnv.js
@@ -3,11 +3,11 @@ const { cleanEnv, str, port } = require("envalid");
 
 const validateEnv = () => {
   cleanEnv(process.env, {
-    NODE_ENV: str(),
-    PORT: port(),
-    MONGO_HOST: str(),
-    MONGO_PORT: str(),
-    MONGO_DATABASE: str(),
+    // NODE_ENV: str(),
+    // PORT: port(),
+    // MONGO_HOST: str(),
+    // MONGO_PORT: str(),
+    // MONGO_DATABASE: str(),
     JWT_SECRET: str(),
     // BOOK_RENTAL_SERVICE_URL: str(),
   });
